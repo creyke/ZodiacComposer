9aaf81ffcfd0c98aa8c2b5b0da772ca43eddc376
Roger Creyke <roger.creyke@odds-bods.com>
2020-12-14 12:38:17 +0000

    Added table generation.

--- index.js
+++ index.js
@@ -87,3 +87,19 @@ encoded.map(e => {
 	}
 	mapId++;
 });
+
+function makeTable(array) {
+    var table = document.createElement('table');
+    for (var i = 0; i < array.length; i++) {
+        var row = document.createElement('tr');
+        for (var j = 0; j < array[i].length; j++) {
+            var cell = document.createElement('td');
+            cell.textContent = array[i][j].c;
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+    return table;
+}
+
+document.getElementsByTagName('body')[0].appendChild(makeTable(grid));
